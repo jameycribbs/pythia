950f0d9a4adc0573fd9bb58db7ad251106d9f7ce
Jamey Cribbs <jamey.cribbs@gmail.com>
2015-03-13 10:23:03 -0400

    redo user handler

--- pythia.go
+++ pythia.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/jameycribbs/pythia/db"
 	"github.com/jameycribbs/pythia/handlers/answers_handler"
+	"github.com/jameycribbs/pythia/handlers/users_handler"
 	"net/http"
 	"regexp"
 )
 
-var validPath = regexp.MustCompile("^/(answer/|answers/)*(new|create|edit|save|delete|view|index)*/*([a-zA-Z0-9]*)$")
+var validPath = regexp.MustCompile("^/(answers/|answer|users|user/)*(new|create|edit|save|delete|view|index)*/*([a-zA-Z0-9]*)$")
 
 func main() {
 	myDB, err := db.OpenDB("data")
@@ -26,6 +27,13 @@ func main() {
 	http.HandleFunc("/answer/edit/", makeHandler(answers_handler.Edit, myDB))
 	http.HandleFunc("/answer/save/", makeHandler(answers_handler.Save, myDB))
 	http.HandleFunc("/answer/delete/", makeHandler(answers_handler.Delete, myDB))
+	http.HandleFunc("/users/", makeHandler(users_handler.Index, myDB))
+	http.HandleFunc("/user/view/", makeHandler(users_handler.View, myDB))
+	http.HandleFunc("/user/new/", makeHandler(users_handler.New, myDB))
+	http.HandleFunc("/user/create/", makeHandler(users_handler.Create, myDB))
+	http.HandleFunc("/user/edit/", makeHandler(users_handler.Edit, myDB))
+	http.HandleFunc("/user/save/", makeHandler(users_handler.Save, myDB))
+	http.HandleFunc("/user/delete/", makeHandler(users_handler.Delete, myDB))
 	http.HandleFunc("/", makeHandler(answers_handler.Index, myDB))
 
 	http.ListenAndServe(":8080", nil)
